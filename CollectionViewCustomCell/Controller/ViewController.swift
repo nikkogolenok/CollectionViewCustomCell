8f2909380e1b7f140d0a17091b42b9aa9cdf56e4
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-24 12:46:28 +0300

    Initial Commit

---
+++ CollectionViewCustomCell/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CollectionViewCustomCell
+//
+//  Created by Никита Коголенок on 24.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
